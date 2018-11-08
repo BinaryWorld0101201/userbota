ce316ace44f753c99efddb0ba22b80f010b5021a
Lenart8274 <44343581+Lenart8274@users.noreply.github.com>
2018-11-08 19:30:28 +0100

    Update _config.php

--- _config.php
+++ _config.php
@@ -5,5 +5,5 @@ $leggi_messaggi_in_uscita = true;
 $lista_admin = [
   40955937,  //id di Bruno :D
   101374607,  //id del creatore di MadelineProto :D
-  12344567,  //un id probabilmente inesistente
+  771511654,  //un id probabilmente inesistente
 ];
